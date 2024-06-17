b77033bd10600bcca3d4efb812fabddd284f995c
Felipe <felipeferreirafacul96@gmail.com>
2024-06-17 16:24:49 -0300

    Modificação Estrutura

--- Web Scraping - Livro/Capitulo 1/aula1.py
+++ Web Scraping - Livro/Capitulo 1/aula1.py
@@ -1,7 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-from requests import HTTPError
-import pandas
 
 html = requests.get('http://pythonscraping.com/pages/page1.html')
 bs = BeautifulSoup(html.content, 'html.parser')
