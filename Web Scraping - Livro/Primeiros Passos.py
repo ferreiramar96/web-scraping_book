76f6eb5a459a2fe75ec3c07e7c8fd3cb999aa11a
Felipe <felipeferreirafacul96@gmail.com>
2024-06-17 11:26:16 -0300

    Primeira Alteração

---
+++ Web Scraping - Livro/Primeiros Passos.py
@@ -0,0 +1,7 @@
+import requests
+from bs4 import BeautifulSoup
+from requests import HTTPError
+
+html = requests.get('http://pythonscraping.com/pages/page1.html')
+bs = BeautifulSoup(html.content, 'html.parser')
+print(bs.h1)
