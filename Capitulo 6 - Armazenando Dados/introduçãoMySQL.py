777835dfea31e61fed13e5bb3dba490e563bb743
Felipe <felipeferreirafacul96@gmail.com>
2024-07-03 20:30:47 -0300

    Últimas Modificações

---
+++ Capitulo 6 - Armazenando Dados/introduçãoMySQL.py
@@ -0,0 +1,12 @@
+import pymysql
+
+conn = pymysql.connect(host='127.0.0.1', unix_socket='/tmp/mysql.sock',
+user='root', passwd=None, db='mysql')
+
+cur = conn.cursor()
+cur.execute('USE scraping')
+cur.execute('SELECT * FROM pages WHERE id=1')
+print(cur.fetchone())
+cur.close()
+conn.close() #É SEMPRE importante fechar as conexões
+             #Para o banco de dados não cair
